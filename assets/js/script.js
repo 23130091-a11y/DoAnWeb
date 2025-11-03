ac752b22ae45f690bba2ab48707cac0296a60ca4
itlord23 <nguyenthihonghanh5098@gmail.com>
2025-11-03 23:01:53 +0700

    Code html, css trang product.html và chỉnh sửa chia file html, css tái sử dụng

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 /* Script slider */
-  const slides = document.querySelectorAll("#slider .slide");
-  let index = 0;
+const slides = document.querySelectorAll("#slider .slide");
+let index = 0;
+  /* Kiểm tra trong code html có #slider mới active */
+if (slides.length > 0) {
+  slides[index].classList.add("active");
 
   function nextSlide() {
     slides[index].classList.remove("active");
     index = (index + 1) % slides.length;
     slides[index].classList.add("active");
-  };
+  }
 
-  slides[index].classList.add("active");
   setInterval(nextSlide, 3000);
+}
+
+/* Load header */
+function loadComponent(id, path) {
+  fetch(path)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    })
+    .catch(err => console.error("Error loading component:", err));
+}
+loadComponent("header", "./layout/header.html");
